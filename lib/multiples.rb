5bf32e83abd1fee77c4da0dbb5125df0a680b5b1
Jesse <homestarflyer@gmail.com>
2016-02-04 17:09:29 +0000

    nitrous-backup at Thu Feb  4 17:09:29 UTC 2016

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,16 @@
 # Enter your procedural solution here!
+
+def collect_multiples(limit)
+  mlts = []
+  while limit > 2
+    limit -= 1
+    mlts.unshift(limit) if limit % 5 == 0 || limit % 3 == 0
+  end
+  return mlts
+end
+
+  def sum_multiples(limit)
+    sum = 0
+    mlts.each {|i| sum + i }
+    return sum
+  end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,17 @@
 # Enter your object-oriented solution here!
+class Multiples
+  def collect_multiples(limit)
+    mlts = []
+    while limit > 2
+      limit -= 1
+      mlts.unshift(limit) if limit % 5 == 0 || limit % 3 == 0
+    end
+    return mlts
+  end
+
+  def sum_multiples(limit)
+    sum = 0
+    collect_multiples(limit).each {|i| sum + i }
+    return sum
+  end
+end
